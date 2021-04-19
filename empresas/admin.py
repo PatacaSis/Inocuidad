1810914d3fbbe804e032a9bab6927adbfa81cc47
PatacaSis <patacasis@gmail.com>
2021-04-19 19:29:50 -0300

    agregado de modelo GrupoTecnologico y Auditoria

--- empresas/admin.py
+++ empresas/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Empresa,Producto
+from .models import Empresa,Producto,GrupoTecnologico,Auditoria
+
+
+class AuditoriaAdmin(admin.ModelAdmin):
+    list_display = ('empresa','fecha','motivo','estado')
 
 class EmpresaAdmin(admin.ModelAdmin):
     list_display = ('nombre','numero','localidad')
 
+class GrupoTecnologicoAdmin(admin.ModelAdmin):
+    list_display = ('numero','descripcion')
+
 class ProductosAdmin(admin.ModelAdmin):
     list_display = ('tipo', 'nombre', 'codigo')
 
-
+admin.site.register(Auditoria,AuditoriaAdmin)
 admin.site.register(Empresa,EmpresaAdmin)
+admin.site.register(GrupoTecnologico,GrupoTecnologicoAdmin)
 admin.site.register(Producto,ProductosAdmin)
--- empresas/models.py
+++ empresas/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+class GrupoTecnologico(models.Model):
+    numero = models.CharField(verbose_name='Grupo/Subgrupo', max_length=5, unique=True)
+    descripcion = models.CharField(verbose_name='Grupo/Subgrupo', max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'Grupo Tecnologico'
+        verbose_name_plural = 'Grupos Tecnologicos'
+        ordering = ['numero']    
+
+    def __str__(self):
+        return '{} - {}'.format(self.numero,self.descripcion)
+
+
 class Producto(models.Model):
     tipo = models.CharField(verbose_name='Tipo producto', max_length=100)
     nombre = models.CharField(verbose_name='Nombre producto', max_length=100)
@@ -18,6 +31,7 @@ class Empresa(models.Model):
     numero = models.CharField(verbose_name='Numero', max_length=20, unique=True)
     localidad = models.CharField(verbose_name='Localidad', max_length=50)
     domicilio = models.CharField(verbose_name='Ubicacion', max_length=50)
+    grupos_id = models.ManyToManyField(GrupoTecnologico)
     productos_id = models.ManyToManyField(Producto)
 
     class Meta:
@@ -27,4 +41,25 @@ class Empresa(models.Model):
 
     def __str__(self):
         return '{} - {}'.format(self.nombre,self.numero)
+
+
+class Auditoria(models.Model):
+    fecha = models.DateField()
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
+    motivo = models.CharField(verbose_name='Motivo de la auditoria', max_length=150)
+    criterio = models.CharField(verbose_name='Criterios de auditoria', max_length=250)
+    alcance = models.CharField(verbose_name='Alcance de auditoria', max_length=250)
+    evidencia = models.TextField(verbose_name='Evidencias encontradas')
+    nc = models.TextField(verbose_name='No conformidades/Conclusiones')
+    estado = models.BooleanField(verbose_name='Abierta/Cerrada', default=False)
+
+    class Meta:
+        verbose_name = 'Auditoria'
+        verbose_name_plural = 'Auditorias'
+        ordering = ['fecha']
+    
+    def __str__(self):
+        return '{}'.format(self.motivo)
+
+
      
